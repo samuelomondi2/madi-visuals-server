929ae94041cc135aebe2bff74c36b882e44f595c
Samuel Omondi <omondisam696@gmail.com>
2026-06-06 01:00:27 -0400

    debug

--- controller/user.js
+++ controller/user.js
@@ -15,7 +15,7 @@ exports.register = async (req, res, next) => {
       return res.status(409).json({ message: 'Email already in use' });
     }
 
-    const user = await User.create({ name, email, password, is_verified, role });
+    const user = await User.create({ name, email, password, role });
     res.status(201).json({ message: 'User registered successfully', user });
   } catch (error) {
     next(error);
