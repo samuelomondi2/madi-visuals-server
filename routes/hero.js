7035130b56ada1b3ede06d5ff24361f20676bb8c
Samuel Omondi <omondisam696@gmail.com>
2026-06-13 09:13:09 -0400

    bugfix

--- routes/hero.js
+++ routes/hero.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const controller = require("../controller/hero.controller");
+const heroController = require("../controller/hero");
 
 const router = express.Router();
 
